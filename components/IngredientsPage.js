bfc17cc71c7916ba81223cc954ff5d5fda92620d
pant3ha <psa140@sfu.ca>
2025-03-08 22:57:45 -0800

    recipe-input page done

--- components/IngredientsPage.js
+++ components/IngredientsPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import Input from "../styles/Input.css";
+import "../styles/IngredientsPage.css";
+import "../styles/generateButton.css";
 
 const IngredientsPage = () => {
     const [ingredients, setIngeredients] = useState([]);
     const [input, setInput] = useState("");
+
 
     const handleInputChange = (e) => {
         setInput(e.target.value);
@@ -11,38 +13,49 @@ const IngredientsPage = () => {
 
     const handleKeyPress = (e) => {
         if (e.key == "Enter" && input.trim() !== "") {
-            setIngeredients([...ingredients, input.trim()]);
+            setIngeredients([input.trim(), ...ingredients, ]);
             setInput("");
         }
     };
 
-    const handleRemoveIngredients = (ingredient) => {
-        setIngeredients(ingredients.filter((item) => item !== ingredient));
-    };
+    const handleSaveClick = () => {
+        //call query function in libs
+        window.location.href = "/next-page"; 
+    }
+
 
     return (
         <div className="ingredients-container">
-            <h1>ingredients</h1>
-            <input
-                type="text"
-                value={input}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyPress}
-                placeholder="type an ingredient"
-            />
-      <div className="ingredient-list">
-        {ingredients.map((ingredient, index) => (
-          <div
-            key={index}
-            className="ingredient-bubble"
-            onClick={() => handleRemoveIngredient(ingredient)}
-          >
-            {ingredient}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+          <h1>Fill your fridge</h1>
+          <input
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+            placeholder="Enter an ingredient"
+            className="input"
+          />
+          <section className="section">
+            {ingredients.map((ingredient, index) => (
+                <div key={index} className="card">
+                    <div className="card-content">
+                        <img src={"/red-apple-transparent-png.webp"} alt="apple" style={{ width: '30px', height: '30px' }}/>      
+                        <div className="card-top">
+                            {ingredient}
+                        </div>
+                    </div>
+                </div>
+            ))}
+          </section>
+          <button className="button" onClick={handleSaveClick}>
+          <div className="shadow"></div>
+                <div className="edge"></div>
+                <div className="front">
+                    <span>Generate</span>
+                </div>
+                </button>
+        </div>
+    );
 };
-
+    
 export default IngredientsPage;
